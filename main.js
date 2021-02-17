40d9667c9ae96e0f0903e3923b8b05472e019fda
Keirarenee <71294118+Keirarenee@users.noreply.github.com>
2021-02-16 20:56:55 -0500

    Add files via upload
    uploaded the console.log

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function age_calculator(member, name, age) {
+    return `My ${member} ${name} is ${age*12} months old!`;
+}
+
+  console.log(age_calculator("Mother", "Joli", "48"))
+
+  console.log(age_calculator("Brother", "Skyler", "28"))
+
+  console.log(age_calculator("Brother", "Oliver", "20"))
